7e05903dde982a522da3da5bb5312821b07a442a
Deepansh Bhargava <deepansh940@gmail.com>
2024-01-12 17:01:08 +0530

    feat: added upload

--- src/api/return/routes/return.js
+++ src/api/return/routes/return.js
@@ -1,9 +1,43 @@
-'use strict';
+"use strict";
 
 /**
  * return router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::return.return');
+const defaultRouter = createCoreRouter("api::return.return");
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+// const extraRoutes = [
+//   {
+//     method: "POST",
+//     path: "/returns/generate",
+//     handler: "api::return.return.generateOTP",
+//     config: {
+//       auth: false,
+//     },
+//   },
+//   {
+//     method: "POST",
+//     path: "/returns/confirm",
+//     handler: "api::return.return.confirmOTP",
+//     config: {
+//       auth: false,
+//     },
+//   },
+// ];
+
+module.exports = customRouter(defaultRouter, []);
